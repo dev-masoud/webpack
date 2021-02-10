f96d2fb939c3349eff23e3ad3e9f0087079108e6
masoud <you@example.com>
2021-02-10 15:07:17 +0330

    first js

---
+++ index.js
@@ -0,0 +1,19 @@
+var esm = "Masoud"
+console.log(esm);
+console.log(typeof esm);
+console.log(esm.charAt(3));
+console.log(esm.charCodeAt(0));
+
+
+let age = 21
+console.log(age);
+console.log(typeof age);
+
+const tt = true
+console.log(tt);
+console.log(typeof tt);
+
+let x = 5, u=true
+let z = x + u
+console.log(z);
+console.log(typeof z);
