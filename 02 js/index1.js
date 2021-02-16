38c5d12a1d36dd657b8d3d71ec335599d92200a4
masoud <you@example.com>
2021-02-16 21:59:23 +0330

    jalase 2

---
+++ 02 js/index1.js
@@ -0,0 +1,29 @@
+let masoud = "masoud rezaie"
+console.log(masoud);
+let res = masoud.replace("rezaie","dragon");
+console.log(res);
+
+
+
+let ali = "ali bakhtiari"
+console.log(ali);
+let res1 = ali.search("ti");
+console.log(res1);
+
+
+
+let haji = "haji baghal"
+console.log(haji);
+let res2 = haji.slice(0, 3)
+console.log(res2);
+
+
+
+let b=1
+for(b ;b<11 ; b++){
+  let c = "*"
+  console.log(c.repeat(b));
+}
+
+
+
